744a8f9b7ea15e25e320f962ae4bed7e1f069476
sempeck <jarek.sepek@mediorama.pl>
2015-10-14 16:52:54 +0200

    Zadanie 1

---
+++ zad1.pas
@@ -0,0 +1,19 @@
+program zad1;
+
+var
+	imie, nazwisko: String;
+	rok, wiek: Integer;
+
+begin
+	WriteLn('Jak masz na imie?');
+	ReadLn(imie);
+	WriteLn('Jak masz na nazwisko?');
+	ReadLn(nazwisko);
+	WriteLn('Podaj swoj wiek:');
+	ReadLn(wiek);
+	WriteLn('Który mamy rok?');
+	ReadLn(rok);
+	WriteLn('Masz na imie ', imie, ', a na nazwisko ', nazwisko, '.');
+	WriteLn('Jesli mamy rok ', rok, ', to urodziles sie w ', rok - wiek, ' roku.');
+    ReadLn;
+end.
