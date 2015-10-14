418994d375a8dd673c9cf0bc43f3530eff1a62bb
sempeck <jarek.sepek@mediorama.pl>
2015-10-14 17:02:32 +0200

    Zadanie 2

---
+++ zad2.pas
@@ -0,0 +1,14 @@
+program zad2;
+
+var
+	n, m : Integer;
+
+begin
+	writeln('Podaj N:');
+	readLn(n);
+	writeln('Podaj M:');
+	readln(m);
+	writeln('N - M = ', n-m);
+	writeln('N + M = ', n+m);
+	readln;
+end.
